4ad20d8b1966f70c836ff1d60cdaa5b5e1abaac7
Oleh-seacrh <oleg.yunyk@gmail.com>
2025-05-03 15:22:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -133,6 +133,14 @@ if start and query:
         gc = get_gsheet_client()
         sh = gc.open_by_key(GSHEET_SPREADSHEET_ID)
 
+                # Створення або відкриття вкладки "Пошуки"
+        try:
+            search_log_sheet = sh.worksheet("Пошуки")
+        except:
+            search_log_sheet = sh.add_worksheet(title="Пошуки", rows="1000", cols="5")
+            search_log_sheet.append_row(["Ключові слова", "Назва", "Посилання", "GPT-відповідь", "Сторінка"])
+
+
         try:
             sheet = sh.worksheet(tab_name)
         except:
