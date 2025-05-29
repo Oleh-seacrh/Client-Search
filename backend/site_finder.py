8215f305ce39ace3f6d75983912ecd4f369d4d49
Oleg Yunyk <oleg.yunyk@gmail.com>
2025-05-29 21:31:39 +0300

    Update site_finder.py

--- backend/site_finder.py
+++ backend/site_finder.py
@@ -2,6 +2,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import requests
+import streamlit as st
 import pandas as pd
 from urllib.parse import urlparse
 
