113067f6dae5e235b83743df0f43300aef928a6b
Oleg Yunyk <oleg.yunyk@gmail.com>
2025-05-29 20:58:02 +0300

    Update gpt_analyzer.py

--- backend/gpt_analyzer.py
+++ backend/gpt_analyzer.py
@@ -1,4 +1,5 @@
 import re
+from backend.utils import call_gpt
 from backend.gsheet_service import get_gsheet_client
 from prompts.prompts import prompt_get_category, prompt_is_potential_client
 from backend.utils import gpt_call  # wrap для client.chat.completions.create
